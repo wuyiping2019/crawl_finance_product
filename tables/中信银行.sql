71b0d6226b3509f4e51b9df431be8d53eb359ebf
wuyiping2019 <48320728+wuyiping2019@users.noreply.github.com>
2022-10-11 10:25:35 +0800

    -m init

--- tables/中信银行.sql
+++
@@ -1,9 +0,0 @@
-select *
-from IP_BANK_ZXYH_pc;
-select max(logid)
-from IP_BANK_ZXYH_mobile a;
-select * from IP_BANK_ZXYH_mobile order by logId desc;
-truncate table IP_BANK_ZXYH_mobile;
-select * from IP_BANK_ZXYH_mobile;
-drop table crawl_zxyh;
-select * from crawl_zxyh;
